ed350cbb43b7d685190181ef194b8768c494bd91
Ryan Baumann <ryan.baumann@gmail.com>
2019-02-04 15:24:57 -0500

    Don't keep all files in memory during PLY conversion

--- e57csv2ply.rb
+++ e57csv2ply.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'csv'
+require 'tempfile'
 
 unless ARGV.length >= 2
   $stderr.puts "Usage: ./e57csv2ply.rb input1.csv [input2.csv input3.csv ...] output.ply"
@@ -8,43 +9,54 @@ unless ARGV.length >= 2
 end
 
 output_filename = ARGV.pop
-vertices = []
-$stderr.puts "Parsing #{ARGV.length} input files..."
-# cartesianX,cartesianY,cartesianZ,intensity,colorRed,colorGreen,colorBlue,rowIndex,columnIndex,cartesianInvalidState
-ARGV.each do |input_filename|
-  $stderr.puts "Parsing #{input_filename}"
-  CSV.foreach(input_filename, :headers => true) do |row|
-    output = {}
-    row_hash = row.to_h
-    output['x'] = row_hash['cartesianX']
-    output['y'] = row_hash['cartesianY']
-    output['z'] = row_hash['cartesianZ']
-    output['intensity'] = row_hash['intensity'].to_f if row_hash['intensity']
-    output['intensity'] = row_hash['Grey'].to_f if row_hash['Grey']
-    output['diffuse_red'] = row_hash['colorRed'].to_i if row_hash['colorRed']
-    output['diffuse_green'] = row_hash['colorGreen'].to_i if row_hash['colorGreen']
-    output['diffuse_blue'] = row_hash['colorBlue'].to_i if row_hash['colorBlue']
-    vertices << output
-  end
-end
-$stderr.puts "Writing output to #{output_filename} (#{vertices.length} vertices)..."
-File.open(output_filename, 'w') do |output|
-  output.puts 'ply'
-  output.puts 'format ascii 1.0'
-  output.puts "element vertex #{vertices.length}"
-  ['x','y','z'].each do |i|
-    output.puts "property float #{i}"
-  end
-  output.puts "property float intensity" if vertices.first.has_key?('intensity')
-  ['red','green','blue'].each do |color|
-    output.puts "property uchar diffuse_#{color}" if vertices.first.has_key?("diffuse_#{color}")
-  end
-  output.puts 'end_header'
-  vertices.each do |vertex|
-    output_elements = []
-    ['x','y','z','intensity','diffuse_red','diffuse_green','diffuse_blue'].each do |element|
-      output_elements << vertex[element] if vertex.has_key?(element)
+tempfile = Tempfile.new()
+puts tempfile.path
+last_vertex = nil
+begin
+  vertices = 0
+  $stderr.puts "Parsing #{ARGV.length} input files..."
+  # cartesianX,cartesianY,cartesianZ,intensity,colorRed,colorGreen,colorBlue,rowIndex,columnIndex,cartesianInvalidState
+  ARGV.each do |input_filename|
+    $stderr.puts "Parsing #{input_filename}"
+    CSV.foreach(input_filename, :headers => true) do |row|
+      output = {}
+      row_hash = row.to_h
+      output['x'] = row_hash['cartesianX']
+      output['y'] = row_hash['cartesianY']
+      output['z'] = row_hash['cartesianZ']
+      output['intensity'] = row_hash['intensity'].to_f if row_hash['intensity']
+      output['intensity'] = row_hash['Grey'].to_f if row_hash['Grey']
+      output['diffuse_red'] = row_hash['colorRed'].to_i if row_hash['colorRed']
+      output['diffuse_green'] = row_hash['colorGreen'].to_i if row_hash['colorGreen']
+      output['diffuse_blue'] = row_hash['colorBlue'].to_i if row_hash['colorBlue']
+      output_elements = []
+      ['x','y','z','intensity','diffuse_red','diffuse_green','diffuse_blue'].each do |element|
+        output_elements << output[element] if output.has_key?(element)
+      end
+      tempfile.puts output_elements.join(' ')
+      vertices += 1
+      last_vertex = output
     end
-    output.puts output_elements.join(' ')
   end
+  tempfile.close
+
+  $stderr.puts "Writing output to #{output_filename} (#{vertices} vertices)..."
+  File.open(output_filename, 'w') do |output|
+    output.puts 'ply'
+    output.puts 'format ascii 1.0'
+    output.puts "element vertex #{vertices}"
+    ['x','y','z'].each do |i|
+      output.puts "property float #{i}"
+    end
+    output.puts "property float intensity" if last_vertex.has_key?('intensity')
+    ['red','green','blue'].each do |color|
+      output.puts "property uchar diffuse_#{color}" if last_vertex.has_key?("diffuse_#{color}")
+    end
+    output.puts 'end_header'
+    File.foreach(tempfile.path) do |line|
+      output.puts line
+    end
+  end
+ensure
+  tempfile.unlink
 end
